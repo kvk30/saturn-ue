90adeace9650d2b188a1503c64c1e988aabe4490
kvk30 <varunkaja.1234@gmail.com>
2024-03-25 02:26:43 +0530

    Updated sidebar

--- src/components/LeftSidebar.jsx
+++ src/components/LeftSidebar.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import './LeftSidebar.css'; // Import the CSS file
 
@@ -6,50 +5,54 @@ function LeftSidebar() {
   return (
     
       <div className="left-sidebar">
-        
-        <ul className="menu-items">
         <button className="write-post-button">
           <span className="fas fa-pencil-alt"></span> Write Post
         </button>
-          
+        <ul className="menu-items">
+          <li>
             <span className="fas fa-bolt"></span> Post Generator
-          
-          
+          </li>
+          <li>
             <span className="fas fa-lightbulb"></span> Ideas Generator
-          
-          
+          </li>
+          <li>
             <span className="fas fa-images"></span> Carousel Maker
-          
-          
+          </li>
+          <li>
             <span className="fas fa-newspaper"></span> Posts
-          
-          
+          </li>
+          <li>
             <span className="fas fa-chart-line"></span> Content Inspiration
-          
-          
+          </li>
+          <li>
             <span className="fas fa-user-clock"></span> Posts for You
-          
-          
+          </li>
+          <li>
             <span className="fas fa-calendar-alt"></span> Schedule
-          
+          </li>
         </ul>
         <div className="preferences">
           <ul>
-             <span className="fas fa-comment-dots"></span> Preferences
+            <li>
+              <span className="fas fa-comment-dots"></span> Preferences
+            </li>
+            <li>
               <span className="fas fa-comment-dots"></span> Feature Request
-            
+            </li>
           </ul>
         </div>
         <div className="word-count">
-          <p>Words generated <i className="fas fa-info-circle"></i></p>
+        <div className="word-count-info">
+          <p>Words generated <i className="fas fa-info-circle"></i></p><p className="word-count-value">1.25k / 50k</p>
+          
+          </div>
           <progress value="1.25" max="50"></progress>
-          <p>1.25k / 50k</p>
           <p>Monthly usage resets in 29 days</p>
+          <br></br>
           <button className="upgrade-plan-button">
-          <span className="fas fa-bolt"></span> Upgrade Plan
-        </button>
+            <span className="fas fa-bolt"></span> Upgrade Plan
+          </button>
         </div>
-        
       </div>
     
   );
